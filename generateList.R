9fa8a876708d1f0777867943f1efe1791dc895a9
Till Dettmering <prinzipiell@gmail.com>
2014-05-17 14:26:27 +0200

    Shortened code

--- generateList.R
+++ generateList.R
@@ -3,15 +3,11 @@
 generateList <- function(src, columns, rm.empty = TRUE, row.no = FALSE) {
   df <- as.data.frame(table(src[,columns]))
 
-  if (rm.empty == TRUE) {
-    df <- subset(df, Freq > 0)
-  }
-
+  if (rm.empty == TRUE) df <- subset(df, Freq > 0)
+  
   colnames(df)[1:(length(columns) + 1)] <- c(columns, 'n')
   
-  if (row.no == TRUE) {
-    df$row.no <- rownames(df)
-  }
+  if (row.no == TRUE) df$row.no <- rownames(df)
   
   return (df)
 }
