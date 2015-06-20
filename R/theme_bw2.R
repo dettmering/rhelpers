fd2d9ba97624dceeac6e5d30352c8fc639fb9fde
Till Dettmering <prinzipiell@gmail.com>
2015-06-20 12:08:19 +0200

    Removed grid lines from bw2 theme

--- R/theme_bw2.R
+++ R/theme_bw2.R
@@ -10,8 +10,8 @@ theme_bw2 <- function(base_size = 12, base_family = "") {
                         legend.key        = element_rect(colour = "grey80"),
                         panel.background  = element_rect(fill = "white", colour = NA),
                         panel.border      = element_rect(fill = NA, colour = "black", size = 1),
-                        panel.grid.major  = element_line(colour = "grey90", size = 0.2),
-                        panel.grid.minor  = element_line(colour = "grey98", size = 0.5),
+                        panel.grid.major  = element_blank(),
+                        panel.grid.minor  = element_blank(),
                         strip.background  = element_rect(fill = "grey80", colour = "grey50", size = 0.2)
                 )
 }
